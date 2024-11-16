5ed47394397b448eaee857ced3e33118e3c2d860
Jannik <1337741710@qq.com>
2024-11-16 15:51:24 +0800

    fix: divider组件返工

--- packages/versakit-ui/components/divider/index.vue
+++ packages/versakit-ui/components/divider/index.vue
@@ -17,21 +17,23 @@ const props = defineProps({
 
 <template>
   <div
-    class="t-divider"
-    :class="{ 't-vertical': direction === 'vertical' }"
+    class="ver-divider"
+    :class="{ 'ver-vertical': direction === 'vertical' }"
     :style="{
       'justify-content': position,
       'border-top': `1px ${line} #dcdfe6`,
     }"
   >
-    <div class="t-divider-title" v-if="position">
+    <div class="ver-divider-title" v-if="position">
       <slot></slot>
     </div>
   </div>
 </template>
 
-<style scoped>
-.t-divider {
+<style lang="scss" scoped>
+@use "../../../style/color/index.scss" as *;
+
+.ver-divider {
   width: 100%;
   height: 1px;
   margin: 24px 0;
@@ -42,7 +44,7 @@ const props = defineProps({
   box-sizing: border-box;
 }
 
-.t-vertical {
+.ver-vertical {
   display: inline-block;
   width: 1px;
   height: 1em;
@@ -52,9 +54,15 @@ const props = defineProps({
   padding: 0;
 }
 
-.t-divider-title {
-  border-left: 20px solid white;
-  border-right: 20px solid white;
-  background-color: white;
+.ver-divider-title {
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  background-color: transparent;
+
+  .dark & {
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    background-color: transparent;
+  }
 }
 </style>

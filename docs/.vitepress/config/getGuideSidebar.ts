3415c2a44dcdad72d6c08473cdc440536a4f0af0
Jannik <1337741710@qq.com>
2024-11-18 14:09:25 +0800

    docs: 更新文档

--- docs/.vitepress/config/getGuideSidebar.ts
+++ docs/.vitepress/config/getGuideSidebar.ts
@@ -4,7 +4,10 @@ export const getGuideSidebar = (): DefaultTheme.Sidebar => {
   return [
     {
       text: "指南",
-      items: [{ text: "Dark 暗黑模式", link: "/guide/dark/" }],
+      items: [
+        { text: "Log 更新日志", link: "/guide/log/" },
+        { text: "Dark 暗黑模式", link: "/guide/dark/" },
+      ],
     },
   ];
 };

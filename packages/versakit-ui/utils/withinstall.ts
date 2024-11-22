1831f16d197facf21b64c399d09c2112efc9f7b7
Jannik <1337741710@qq.com>
2024-11-22 23:24:00 +0800

    fix: withIntall方法类型完善

--- packages/versakit-ui/utils/withinstall.ts
+++ packages/versakit-ui/utils/withinstall.ts
@@ -1,11 +1,11 @@
-import type { Plugin } from 'vue'
+import type { App, Plugin } from 'vue'
 
 export type SFCWithInstall<T> = T & Plugin
 export const withInstall = <T, E extends Record<string, any>>(
   main: T,
   extra?: E,
 ) => {
-  ;(main as SFCWithInstall<T>).install = (app): void => {
+  ;(main as SFCWithInstall<T>).install = (app: App): void => {
     for (const comp of [main, ...Object.values(extra ?? {})]) {
       app.component(comp.name, comp)
     }

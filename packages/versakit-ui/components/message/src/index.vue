bef41c5563c5636f9af32d79b75bbe817295cc04
Jannik <1337741710@qq.com>
2024-12-04 01:44:39 +0800

    feat: Message组件重置WIP中

--- packages/versakit-ui/components/message/src/index.vue
+++ packages/versakit-ui/components/message/src/index.vue
@@ -1,6 +1,8 @@
 <template>
   <transition name="down" @after-leave="destroy">
     <div :class="VerClass" v-show="isVisable">
+      <!-- 根据type的值来动态选择图标 -->
+      <VerIcon :color="getIconColor" :name="getIconName" />
       <span class="text">{{ content }}</span>
     </div>
   </transition>
@@ -9,8 +11,29 @@
 <script setup lang="ts">
 import { onMounted, ref, computed } from 'vue'
 import type { MessageProps } from './type'
+import { VerIcon } from '../../../index'
 
 defineOptions({ name: 'VerMessage' })
+
+// 定义图标类型与对应名称、颜色的映射关系，方便扩展和维护
+const iconMap = {
+  success: {
+    name: 'passed',
+    color: 'green',
+  },
+  danger: {
+    name: 'clear',
+    color: 'red',
+  },
+  warning: {
+    name: 'warning',
+    color: 'orange',
+  },
+  info: {
+    name: 'info',
+    color: '#c4c4c4',
+  },
+}
 
 const isVisable = ref(false)
 
@@ -22,7 +45,17 @@ const props = withDefaults(defineProps<MessageProps>(), {
 })
 
 const VerClass = computed(() => {
-  return ['ver-message', props.type == '' ? '' : `ver-message-${props.type}`]
+  return ['ver-message']
+})
+
+// 根据传入的type获取对应的图标名称
+const getIconName = computed(() => {
+  return iconMap[props.type]?.name || 'default-icon-name' // 设置默认图标名称，防止不存在的类型情况
+})
+
+// 根据传入的type获取对应的图标颜色
+const getIconColor = computed(() => {
+  return iconMap[props.type]?.color || 'gray' // 设置默认图标颜色，防止不存在的类型情况
 })
 
 /**

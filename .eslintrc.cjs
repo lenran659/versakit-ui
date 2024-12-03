c42f77fe8a7e0e3f2b3c1945646db3fdf76551bc
裴雨孜 <2171204141@qq.com>
2024-12-03 21:53:59 +0800

    fix: 处理提交冲突

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ module.exports = {
   /* 继承已有的规则 */
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -54,5 +54,9 @@ module.exports = {
     'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
     'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
     'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
+  },
+  globals: {
+    withDefaults: 'readonly', // 声明 `withDefaults` 为全局只读变量
+    defineProps: 'readonly',
   },
 }

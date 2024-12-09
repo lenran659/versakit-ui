05254a99c58b37ae9cf249923a709be32175f297
Jannik <1337741710@qq.com>
2024-12-09 14:47:20 +0800

    feat: 部分组件测试

--- packages/ui/components/formItem/test/index.spec.ts
+++ packages/ui/components/formItem/test/index.spec.ts
@@ -5,6 +5,7 @@ import VerFormItem from '../src/index.vue'
 describe('form-item', () => {
   it('测试是否有默认样式', () => {
     const wrapper = mount(VerFormItem)
+
     expect(wrapper.classes()).toContain('ver-form-item')
   })
 })

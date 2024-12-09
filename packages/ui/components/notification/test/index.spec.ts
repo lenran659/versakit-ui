5163af1c3ef905dcfffb8ec3cbbccdeb4a3b63c8
Jannik <1337741710@qq.com>
2024-12-09 13:45:58 +0800

    feat: Notification组件单测完毕

--- packages/ui/components/notification/test/index.spec.ts
+++ packages/ui/components/notification/test/index.spec.ts
@@ -18,4 +18,61 @@ describe('notification', () => {
     })
     expect(wrapper.exists()).toBe(true)
   })
+
+  const type = ['success', 'warning', 'info', 'error']
+  type.forEach((type) => {
+    it(`测试${type}`, () => {
+      const props = {
+        type: type,
+        plain: false,
+        title: 'title',
+        content: 'Test message',
+      }
+
+      const wrapper = mount(VerNotifcation, {
+        props: {
+          props,
+        },
+      })
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+
+  const typePlain = ['success', 'warning', 'info', 'error']
+  typePlain.forEach((type) => {
+    it(`测试${type}-plain`, () => {
+      const props = {
+        type: type,
+        plain: true,
+        title: 'title',
+        content: 'Test message',
+      }
+
+      const wrapper = mount(VerNotifcation, {
+        props: {
+          props,
+        },
+      })
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+
+  const position = ['top-right', 'top-left', 'bottom-left', 'bottom-right']
+  position.forEach((position) => {
+    it(`测试位置${position}`, () => {
+      const props = {
+        plain: true,
+        title: 'title',
+        content: 'Test message',
+        position,
+      }
+
+      const wrapper = mount(VerNotifcation, {
+        props: {
+          props,
+        },
+      })
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
 })

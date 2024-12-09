9a264deab0aae68f8941889cd5d739888181d170
Jannik <1337741710@qq.com>
2024-12-09 14:23:05 +0800

    feat: Message组件单测完成

--- packages/ui/components/message/test/index.spec.ts
+++ packages/ui/components/message/test/index.spec.ts
@@ -16,4 +16,40 @@ describe('message', () => {
     })
     expect(wrapper.exists()).toBe(true)
   })
+
+  const type = ['success', 'warning', 'info', 'error']
+  type.forEach((type) => {
+    it(`测试${type}`, () => {
+      const props = {
+        type: type,
+        plain: false,
+        content: 'Test message',
+      }
+
+      const wrapper = mount(VerMessage, {
+        props: {
+          props,
+        },
+      })
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+
+  const typePlain = ['success', 'warning', 'info', 'error']
+  typePlain.forEach((type) => {
+    it(`测试${type}-plain`, () => {
+      const props = {
+        type: type,
+        plain: true,
+        content: 'Test message',
+      }
+
+      const wrapper = mount(VerMessage, {
+        props: {
+          props,
+        },
+      })
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
 })

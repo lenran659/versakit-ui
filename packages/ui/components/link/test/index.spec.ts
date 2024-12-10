353ae9fa3d6c2dcf8ec8fe9d92f9d0c4a6d29acd
江科贤 <justhappymie@gmail.com>
2024-12-10 22:14:09 +0800

    feat:部分单测Link、dialog、radio、switch

--- packages/ui/components/link/test/index.spec.ts
+++ packages/ui/components/link/test/index.spec.ts
@@ -7,4 +7,17 @@ describe('link', () => {
     const wrapper = mount(VerText)
     expect(wrapper.classes()).toContain('ver-link')
   })
+
+  it('测试传递的属性', () => {
+    const wrapper = mount(VerText, {
+      props: { href: 'https://example.com' },
+    })
+    expect(wrapper.attributes('href')).toBe('https://example.com')
+  })
+
+  it('测试点击事件', async () => {
+    const wrapper = mount(VerText)
+    await wrapper.trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('click')
+  })
 })

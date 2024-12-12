3f15244468a3fb2da128a4932835c29d46444d20
Jannik <1337741710@qq.com>
2024-12-13 00:45:56 +0800

    docs: 布局容器完成

--- packages/docs/components/container/index.md
+++ packages/docs/components/container/index.md
@@ -2,16 +2,16 @@
 
 用于布局的容器组件，方便快速搭建页面的基本结构：
 
-`ver-container`外层容器。当子元素中包含`ver-header`或`t-footer`时，全部子元素会垂直上下排列，否则会水平左右排列。
+`v-container`外层容器。当子元素中包含`v-header`或`v-footer`时，全部子元素会垂直上下排列，否则会水平左右排列。
 
-`ver-header`:顶栏容器。
-`ver-aside`:侧边栏容器。
-`ver-main`:主要区域容器。
-`ver-footer`:底栏容器。
+`v-header`:顶栏容器。
+`v-aside`:侧边栏容器。
+`v-main`:主要区域容器。
+`v-footer`:底栏容器。
 
 > **Tips**
 >
-> 以上组件采用了 flex 布局，使用前请确定目标浏览器是否兼容。 此外， `ver-container`的直接子元素必须是> 后四个组件中的一个或多个。 后四个组件的亲元素必须是一个 `ver-container`
+> 以上组件采用了 flex 布局，使用前请确定目标浏览器是否兼容。 此外， `v-container`的直接子元素必须是> 后四个组件中的一个或多个。 后四个组件的亲元素必须是一个 `v-container`
 
 ## 常见布局
 
---
+++ packages/docs/example/container/partOne.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="common-layout">
+    <v-container class="is-vertical">
+      <v-header>Header</v-header>
+      <v-main>Main</v-main>
+    </v-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { VContainer, VHeader, VMain } from '@versakit/ui'
+</script>
+
+<style scoped>
+.is-vertical {
+  flex-direction: column;
+}
+
+.v-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #c6e2ff;
+}
+.v-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ecf5ff;
+  height: 150px;
+}
+</style>

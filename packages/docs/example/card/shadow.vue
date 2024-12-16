57e52069e3b416ee357d644addbb7fa9177a1a8d
Jannik <1337741710@qq.com>
2024-12-17 02:39:33 +0800

    fix: Card组件ReMaker

--- packages/docs/example/card/shadow.vue
+++ packages/docs/example/card/shadow.vue
@@ -1,17 +1,17 @@
 <template>
   <ver-row>
-    <ver-col :span="6" :offset="2">
-      <ver-card shadow="always">Always</ver-card>
-    </ver-col>
-    <ver-col :span="6" :offset="2">
-      <ver-card shadow="hover">Hover</ver-card>
-    </ver-col>
-    <ver-col :span="6" :offset="2">
-      <ver-card shadow="never">Never</ver-card>
-    </ver-col>
+    <ver-card style="width: 480px" shadow="always">Always</ver-card>
+    <ver-card style="width: 480px" shadow="hover">Hover</ver-card>
+    <ver-card style="width: 480px" shadow="never">Never</ver-card>
   </ver-row>
 </template>
 
 <script lang="ts" setup>
-import { VerCard, VerCol, VerRow } from '@versakit/ui'
+import { VerCard, VerRow } from '@versakit/ui'
 </script>
+
+<style scoped>
+.ver-card {
+  margin: 20px 0;
+}
+</style>
---
+++ packages/docs/example/card/template.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ver-card>
+      <template #header>
+        <div>
+          <span>头部</span>
+        </div>
+      </template>
+      <div>
+        <span>内容</span>
+      </div>
+      <template #footer>
+        <div>
+          <span>底部</span>
+        </div>
+      </template>
+    </ver-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { VerCard } from '@versakit/ui'
+</script>
